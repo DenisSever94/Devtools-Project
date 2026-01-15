80229d1b92669879bd085e0b7dd7e05fe0ba1bd3
Denis Sever <sedihdenis@bk.ru>
2026-01-15 14:59:15 +0300

    fix: class LiveCodingDemo

--- src/main/java/ru/mentee/power/devtools/LiveCodingDemo.java
+++ src/main/java/ru/mentee/power/devtools/LiveCodingDemo.java
@@ -1,4 +1,27 @@
 package ru.mentee.power.devtools;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class LiveCodingDemo {
+  private static final Logger LOG = LoggerFactory.getLogger(LiveCodingDemo.class);
+
+  public static void main(String[] args) {
+    printFizzBuzz(15);
+
+  }
+
+  public static void printFizzBuzz(int n) {
+    for (int i = 1; i < n; i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        LOG.info("FizzBuzz");
+      } else if (i % 3 == 0) {
+        LOG.info("Fizz");
+      } else if (i % 5 == 0) {
+        LOG.info("Buzz");
+      } else {
+        LOG.info("Число ({}) ", i);
+      }
+    }
+  }
 }
