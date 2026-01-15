e09753648789b2f69d1ff4b6ac1a0fc0313cb1a9
Denis Sever <sedihdenis@bk.ru>
2026-01-15 16:53:50 +0300

    feat: class LiveCodingDemo

--- src/main/java/ru/mentee/power/devtools/LiveCodingDemo.java
+++ src/main/java/ru/mentee/power/devtools/LiveCodingDemo.java
@@ -7,12 +7,21 @@ public class LiveCodingDemo {
   private static final Logger LOG = LoggerFactory.getLogger(LiveCodingDemo.class);
 
   public static void main(String[] args) {
+    int[] arrayNumber = {12, 22, 1, 2, 42};
+
     printFizzBuzz(15);
 
+    LOG.info("Сумма всех четных чисел в массиве {}", sumEven(arrayNumber));
+
+    try {
+      LOG.info("Максимальное число в массиве {}", findMax(arrayNumber));
+    } catch (IllegalArgumentException e) {
+      LOG.error(e.getMessage());
+    }
   }
 
   public static void printFizzBuzz(int n) {
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
       if (i % 3 == 0 && i % 5 == 0) {
         LOG.info("FizzBuzz");
       } else if (i % 3 == 0) {
@@ -24,4 +33,29 @@ public class LiveCodingDemo {
       }
     }
   }
+
+  public static int sumEven(int[] numbers) {
+    int sum = 0;
+    for (int number : numbers) {
+      if (number % 2 == 0) {
+        sum += number;
+      }
+    }
+    return sum;
+  }
+
+  public static int findMax(int[] numbers) {
+    if (numbers == null || numbers.length == 0) {
+      throw new IllegalArgumentException("Массив пуст");
+    }
+
+    int max = numbers[0];
+    for (int number : numbers) {
+      if (number > max) {
+        max = number;
+      }
+    }
+    return max;
+  }
 }
+
